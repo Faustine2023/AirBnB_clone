66547e381144c8ecd65280b727b2c1d33b6d32aa
Victor Midega <vbm7752@gmail.com>
2023-12-08 15:15:55 -0800

    State.py

---
+++ models/State.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Documentation Module for the State class"""
+from models.base_model import BaseModel
+
+
+
+
+class State(BaseModel):
+    """Defines Stae as a class model"""
+    name =""
+    address = ""
