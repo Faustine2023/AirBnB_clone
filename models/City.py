48edefcc068cd87b78956f7e41f16b2d5c387d7d
Victor Midega <vbm7752@gmail.com>
2023-12-08 15:22:38 -0800

    new commit

---
+++ models/City.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module Documentation for the City"""
+from models.base_model import BaseModel
+
+
+
+
+
+class City(BaseModel):
+    """Defines the class City""
+    name = ""
+    country = ""
